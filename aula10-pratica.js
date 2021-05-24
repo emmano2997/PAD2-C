7bd2f4c5c84a43b92b27756941914f4237650c27
GersonRS <gersonrs@live.com>
2021-05-24 11:23:50 -0300

    aula 10 atividade

---
+++ aula10-pratica.js
@@ -0,0 +1,27 @@
+// resolvam
+
+// Função recebe como parâmetro um valor numérico x
+// e retorna o seu quadrado.
+function retornaQuadrado(base){
+  return base ** 2;
+}
+
+function retornaResto(dividendo, divisor){
+  return dividendo % divisor;
+}
+
+console.log(retornaResto(6,10));
+
+// 24 = retornaQuadrado(x) - retornaResto(x,y)
+// 24 =         36         - 
+// 24 - 36 = retornaResto(6,72)
+
+// Função recebe como parâmetro as funções retornaQuadrado e retornaResto
+// e retorna o resultado da subtração entre os valores retornados por estas funções, respectivamente.
+let valor1 = 6, valor2 = 60;
+
+function retornaValor(a,b){
+  console.log(a(valor1)-b(valor1,valor2));
+}
+
+retornaValor(retornaQuadrado, retornaResto);
