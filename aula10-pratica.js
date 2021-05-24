0f84bb11d497abfadd48d0544326da75f101e038
Emmanuel A. C. Santos <67324982+emmano2997@users.noreply.github.com>
2021-05-24 12:21:33 -0300

    resolução-aula10pratica
    Bem, pelo meus cálculos...

--- aula10-pratica.js
+++ aula10-pratica.js
@@ -4,20 +4,22 @@
 // Função recebe como parâmetro um valor numérico x
 // e retorna o seu quadrado.
 function retornaQuadrado(base){
-  return base ** 2;
+  return base ** 2; //5^2 = 25
 }
 
 function retornaResto(dividendo, divisor){
   return dividendo % divisor;
+   // 5/2 = 2,5 
+   // 5%2 = 4(1)
 }
 
 // 24 = retornaQuadrado(x) - retornaResto(x,y)
-// 24 =         36         - 
-// 24 - 36 = retornaResto(6,72)
+// 24 =         25         - 
+// 24 - 25 = retornaResto(5,2)
 
 // Função recebe como parâmetro as funções retornaQuadrado e retornaResto
 // e retorna o resultado da subtração entre os valores retornados por estas funções, respectivamente.
-let valor1 = 6, valor2 = 60;
+let valor1 = 5, valor2 = 2;
 
 function retornaValor(a,b){
   console.log(a(valor1)-b(valor1,valor2));
